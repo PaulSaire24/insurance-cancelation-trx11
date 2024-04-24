58224c3101c5d45641aa4b721ad4cc26a551aae7
Sergio Aroni Carbajal <sacarbajal@bbva.com>
2024-04-24 18:13:20 -0500

    fix: [PARSRBVD-2408] modify flow legacy

--- artifact/libraries/RBVDR011IMPL/src/main/java/com/bbva/rbvd/lib/r011/impl/business/CancellationBusiness.java
+++ artifact/libraries/RBVDR011IMPL/src/main/java/com/bbva/rbvd/lib/r011/impl/business/CancellationBusiness.java
@@ -112,22 +112,26 @@ public class CancellationBusiness extends AbstractLibrary {
         }
 
 
-        // Se inserta un registro en la tabla de movimiento de cancelaciones
-        Map<String, Object> requestCancellationMovLast = baseDAO.executeGetRequestCancellationMovLast(input.getContractId());
-        LOGGER.info("CancellationBusiness - cancellationPolicy() - requestCancellationMovLast: {}", requestCancellationMovLast);
-        if(requestCancellationMovLast==null){
-            return null;
-        }
-        // Dato que se saca para enviar en el correo - el id de la secuencia de un seguro en solicitud de cancelacion
-        String requestCancellationId = requestCancellationMovLast.get(RBVDProperties.FIELD_REQUEST_SEQUENCE_ID.getValue()).toString();
-        LOGGER.info("***** CancellationBusiness - executePolicyCancellation requestCancellationId: {} *****", requestCancellationId);
+        String requestCancellationId = "11111";// Default
+        if(!isCancellationLegacyFlow) {
+            // Obtiene datos de la tabla de solicitud y movimiento
+            Map<String, Object> requestCancellationMovLast = baseDAO.executeGetRequestCancellationMovLast(input.getContractId());
+            LOGGER.info("CancellationBusiness - cancellationPolicy() - requestCancellationMovLast: {}", requestCancellationMovLast);
+            if (requestCancellationMovLast == null) {
+                return null;
+            }
+            // Dato que se saca para enviar en el correo - el id de la secuencia de un seguro en solicitud de cancelacion
+            requestCancellationId = requestCancellationMovLast.get(RBVDProperties.FIELD_REQUEST_SEQUENCE_ID.getValue()).toString();
+            LOGGER.info("***** CancellationBusiness - executePolicyCancellation requestCancellationId: {} *****", requestCancellationId);
 
-        boolean isInsertMovCancel = executeCancellationRequestMov(input, requestCancellationMovLast);
-        LOGGER.info("***** CancellationBusiness - executePolicyCancellation isInsertMovCancel: {} *****", isInsertMovCancel);
+            boolean isInsertMovCancel = executeCancellationRequestMov(input, requestCancellationMovLast);
+            LOGGER.info("***** CancellationBusiness - executePolicyCancellation isInsertMovCancel: {} *****", isInsertMovCancel);
 
-        if(!isCancellationLegacyFlow && !ConstantsUtil.BUSINESS_NAME_FAKE_INVESTMENT.equals(productCode) && !isInsertMovCancel){
-            this.addAdvice(RBVDErrors.ERROR_POLICY_CANCELED.getAdviceCode());
-            return null;
+            if(!isCancellationLegacyFlow && !ConstantsUtil.BUSINESS_NAME_FAKE_INVESTMENT.equals(productCode) && !isInsertMovCancel){
+                this.addAdvice(RBVDErrors.ERROR_POLICY_CANCELED.getAdviceCode());
+                return null;
+            }
+
         }
 
         // Se mapeo el correo del cliente
